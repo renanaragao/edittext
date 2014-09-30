af41ca0b044f890ad1301cbed774d9fec21652f6
unknown <renan.aragao@RS-DS03.alfa.local>
2014-09-30 09:27:06 -0300

    Criado o layout padrão.

--- app/app.js
+++ app/app.js
@@ -1,3 +1,8 @@
 (function(){
-	
+	var iframe = document.querySelectorAll('.edittext > .edittext-input > iframe')[0];
+	iframe.contentDocument.designMode = 'on';
+
+	if(iframe.contentWindow)
+		iframe.contentWindow.document.designMode="on";
+
 })();
