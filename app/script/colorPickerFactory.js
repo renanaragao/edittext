8da05872a28576f36590d037faa0ec3bcd8f0f08
unknown <renan.aragao@RS-DS03.alfa.local>
2014-10-01 16:20:40 -0300

    Adicionar botão para fechar o color picker. Template foi movido para o código.

--- app/script/colorPickerFactory.js
+++ app/script/colorPickerFactory.js
@@ -11,6 +11,14 @@ edittext.colorPickerFactory = function(editor, callback){
 
 	ColorPicker.fixIndicators(sliderIndicador, pickerIndicador);
 
+	var btnCancel = editor.getElementsByClassName('button-cancel-color-picker')[0];
+
+	btnCancel.addEventListener('click', function(){
+
+		editor.getElementsByClassName('edittext-color-picker')[0].style.display = 'none';
+
+	}, true);
+
 	return ColorPicker(slider, picker, function(hex, hsv, rgb, pickerCoordinate, sliderCoordinate){
 
 		ColorPicker.positionIndicators(sliderIndicador, pickerIndicador, sliderCoordinate, pickerCoordinate);
