d40be924fedccbb395be3933ae56a6fe057d5938
unknown <renan.aragao@RS-DS03.alfa.local>
2014-10-01 17:15:23 -0300

    CSS minificado

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,19 +3,25 @@ module.exports = function(grunt) {
 
   // Project configuration.
   grunt.initConfig({
-  phk: grunt.file.readJSON('package.json'),
-  concat: {
-	js:{
-	src:['app/plugin/flexiColorPicker/colorpicker.js',
-	'app/script/app.js',
-	'app/script/factory/*.js',
-	'app/script/comandos/*.js',
-	'app/script/editor.js',
-	'app/script/edittext.js',
-	'app/script/start.js'],
-	dest: 'builds/js/app.js'
-	}
-  },
+    phk: grunt.file.readJSON('package.json'),
+    concat: {
+      css: {
+        src: ['app/css/edittext.css',
+              'app/css/icons.css'],
+        dest: 'builds/css/edittext.css'
+      },
+      js: {
+        src: ['app/plugin/flexiColorPicker/colorpicker.js',
+          'app/script/app.js',
+          'app/script/factory/*.js',
+          'app/script/comandos/*.js',
+          'app/script/editor.js',
+          'app/script/edittext.js',
+          'app/script/start.js'
+        ],
+        dest: 'builds/js/app.js'
+      }
+    },
     // Task configuration.
     jshint: {
       options: {
@@ -37,13 +43,19 @@ module.exports = function(grunt) {
         src: 'Gruntfile.js'
       }
     },
-	uglify : {
-      options : {
-        mangle : false
+    cssmin: {
+      css: {
+        src: 'builds/css/edittext.css',
+        dest: 'builds/css/edittext.min.css'
+      }
+    },
+    uglify: {
+      options: {
+        mangle: false
       },
-      my_target : {
-        files : {
-          'builds/js/edittext.min.js' : [ 'builds/js/app.js' ]
+      my_target: {
+        files: {
+          'builds/js/edittext.min.js': ['builds/js/app.js']
         }
       }
     }
@@ -53,8 +65,9 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
 
   // Default task.
-  grunt.registerTask('default', ['jshint', 'concat', 'uglify']);
+  grunt.registerTask('default', ['jshint', 'concat', 'uglify', 'cssmin']);
 
 };
